e98536b82359c024f605018b425f6cdbf07d5599
TYT_AKHILP <akhilise.rymec@gmail.com>
2024-03-02 13:43:33 +0530

    home screen addition

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import TrendingRecipe from './components/Recipe/TrendingRecipe'
 import NavBar from './components/NavBar/NavBar'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import AddRecipe from './components/Recipe/AddRecipe';
+import Home from './components/Home/Home';
+// import { Home } from '@mui/icons-material';
+import "./App.css"
+
 
 function App() {
   return (
 
     <Router>
       <NavBar/>
+      <Home/>
       <Routes>
         <Route path="/" element={<ShareRecipe/>}/>
         <Route path="/recipes" element={<TrendingRecipe/>}/>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,27 @@
 // import React from 'react'
-import "./Home.css"
-// import NavBar from '../NavBar/NavBar'
-
+import "./Home.css";
+import chef from "../../assets/chef.png"
 
 const Home = () => {
   return (
-   <>
-    
-   <div className="containers">
-        {/* <NavBar/> */}
+    <>
+      <div className="containers">
+        <div className="food-data">
+            <div className="content">
+            <h3>Your daily dish a food journey</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Voluptatibus iure excepturi, assumenda veniam aliquam doloremque?
+          </p>
+            </div>
+        </div>
 
-        {/* <div className="food-data">
-        <h3>Your daily dish a food journey</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure excepturi, assumenda veniam aliquam doloremque?</p>
-        <button>Sign Up</button>
-        </div> */}
-   </div>
-   </>
-  )
-}
+        <div className="food-img">
+          <img src={chef} alt="chef_img" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
